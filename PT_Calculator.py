a2ba2c44ae565d4652d51f08f0883ddf418343fd
Sh1rom1 <18521737331@163.com>
2025-02-19 14:54:35 +0000

    Update PT_Calculator.py

--- PT_Calculator.py
+++ PT_Calculator.py
@@ -9,7 +9,16 @@ window = tk.Tk ()  #TK () method to initialise a main window
 window.title ("Calculator")  # set title
 window.geometry ("800x600")  #set calculator window size
 
+entry = tk.Entry(window, font=("Arial", 24), bd=20, relief="groove", justify="right", width=54)
+entry.grid(row=0, column=0, columnspan=4)
 
 
-
+buttons = [
+    ['','','','√',]
+    ['Sin','Cos','Tan','^'],
+    ['7', '8', '9', '/'],
+    ['4', '5', '6', '*'],
+    ['1', '2', '3', '-'],
+    ['C', '0', '=', '+']
+]
 window.mainloop()
