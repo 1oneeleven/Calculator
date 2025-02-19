e288842380e23d2600dbafa5b3a9df50276ed548
1oneeleven <lgnd_247@icloud.com>
2025-02-19 14:18:15 +0000

    set up window and geometry

--- PT_Calculator.py
+++ PT_Calculator.py
@@ -2,7 +2,14 @@
 #the following code is the FC723 portflio assessment 3
 
 import tkinter as tk
- 
-improt math
+import math
 
-#testing 
+
+window = tk.Tk ()  #TK () method to initialise a main window
+window.title ("Calculator")  # set title
+window.geometry ("800x600")  #set calculator window size
+
+
+
+
+window.mainloop()
