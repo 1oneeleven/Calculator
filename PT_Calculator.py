46c4c9b4aa4ba4a68b7fbfd51240a5779a9b0772
1oneeleven <lgnd_247@icloud.com>
2025-02-22 15:43:39 +0000

    FInal Updates and code comments added.

--- PT_Calculator.py
+++ PT_Calculator.py
@@ -2,27 +2,31 @@
 #the following code is the FC723 portflio assessment 3
 
 import tkinter as tk
-import math
+#Import math module
+import math 
+#Import trigonometric and special function from the math module
 from math import sin, cos, tan, asin, acos, atan, sqrt
 
 
+'''Create class for the calculator GUI'''
 class GUICalculator:
     def __init__(self,window):
         
+        #Initialize expression for calculator
         self.expression=""
         self.number_input=tk.StringVar()
-        
-        
+    
+        #Create main window
         self.window=window
-        self.window.title ("Calculator")  # set title
+        self.window.title ("Calculator")  #set title for window
         self.window.geometry ("800x1000")  #set calculator window size
     
         # Create an input box to display the calculation result or input content
         self.entry = tk.Entry(self.window, textvariable=self.number_input, font=("Arial", 24), bd=20, relief="groove", justify="right", width=54)
-        # Place the input box into the window's grid
+        #set position for the input box into the window's grid
         self.entry.grid(row=0, column=0, columnspan=4)
     
-    #Button layout, a martix represents the buttons in each row
+        #Define the matrix button layout of calculator buttons in a 2D list 
         self.buttons = [
             ['arcsin','arccos','arctan','√'],
             ['Sin','Cos','Tan','**2'],
@@ -32,65 +36,76 @@ class GUICalculator:
             ['0', '.', 'Clear', '+'],
             ['(',')','=']
         ]
-        #create a button for each button and put it in the grid
+        
+        #Create and place buttons in calculator window in a grid pattern
         for i, row in enumerate(self.buttons):
             # Create buttons and bind click events to each button
             for j, button_text in enumerate(row):
                 self.button = tk.Button(self.window, text=button_text, font=("Arial", 18), height=4, width=10, command=lambda button_text=button_text: self.on_click(button_text)) 
-                                #The size of the window and the textbox the font
+                #Set grid posttions for buttons
                 self.button.grid(row=i+1, column=j, padx=5, pady=4)
                 
         
     
     def on_click(self, button_text):
-        #add self.number_input to entry widget
+        
+        
+        ''' Try function to handle click events '''
+        
+        
         try:
             if button_text== '=':
-                #evaluate the expression in number_input eval
+                #Evaluate the mathematical expression in number_input
                 self.expression=eval(self.expression)
          
-            #if statemetn to clear entry when "C" is pressed    
+            #Clear expression is "=" button is pressed
             elif button_text=='Clear':
                 self.expression = ""
-     
+                
+            #Calulate Trigonometric and special functions imported from math module
+            #Calculates Sin
             elif button_text == 'Sin':
-                
-                
                 self.expression+='sin'
-                
+            
+            #Calculates Cosine
             elif button_text == 'Cos':
                 self.expression+= 'cos'
              
+            #Calculates Tangent
             elif button_text == 'Tan':
                 self.expression+= 'tan'
     
+            #Calculates Arcsin
             elif button_text == 'arcsin': 
                 self.expression+= 'asin'
                 
-                
+            #Calculates Arccos
             elif button_text == 'arccos':
                 self.expression+= 'acos'
                 
-                
+            #Calculates Arctan
             elif button_text == 'arctan':
-                self.expression+= 'tan'
+                self.expression+= 'atan'
                 
-           
+           #Calculates Squareroot 
             elif button_text == '√':
                 self.expression+= 'sqrt'
             
             
             else:
+                #Keep adding the button inputs to a string in button_text
                 self.expression+=str(button_text)
                 
             
         except:
-            
+            #Handle evaluation errors, could be depending on different math principles
             self.expression = "error"
             
+        #Update the entry widget to the expression following the try function
         self.number_input.set(self.expression)
     
 
+#Create and execute the Calculator window
 window=tk.Tk()
 calculator=GUICalculator(window)
 window.mainloop()
